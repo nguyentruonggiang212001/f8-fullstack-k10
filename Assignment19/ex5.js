771b7ae76fc8d0f8b304b8f8f4b801c977fdbe27
nguyentruonggiang212001 <nguyentruonggiang212001@gmail.com>
2024-09-07 14:21:31 +0700

    nop bai tap

--- Assignment19/ex5.js
+++ Assignment19/ex5.js
@@ -1,6 +1,5 @@
 function printMultiplicationTable() {
   for (let x = 1; x <= 10; x++) {
-    console.log(`Bảng cửu chương ${x}:`);
     for (let y = 1; y <= 10; y++) {
       console.log(`${x} x ${y} = ${x * y}`);
     }
