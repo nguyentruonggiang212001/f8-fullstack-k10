2be75a471b8fafe60a7baaaeeafc7c9b146ab992
nguyentruonggiang212001 <nguyentruonggiang212001@gmail.com>
2024-09-14 15:28:09 +0700

    nop bai tap

---
+++ Assignment20/ex1.js
@@ -0,0 +1,23 @@
+let content = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s`;
+
+let totalWord = content.replaceAll(" ", "</span> <span>");
+totalWord = `<span>${totalWord}</span>`;
+
+const paragraph = document.getElementById("paragraph");
+paragraph.innerHTML = totalWord;
+
+const span = document.getElementsByTagName("span");
+
+function highlightWord() {
+  let currentIndex = 0;
+  let previousIndex = span.length - 1;
+
+  setInterval(() => {
+    span[previousIndex].style.color = "black";
+    span[currentIndex].style.color = "red";
+    previousIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % span.length;
+  }, 1000);
+}
+
+highlightWord();
