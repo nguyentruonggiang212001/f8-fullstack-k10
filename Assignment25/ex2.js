59b65f3401a9d5b1c8ecb61e07a8ff8a6ae3d09c
nguyentruonggiang212001 <nguyentruonggiang212001@gmail.com>
2024-10-01 10:29:12 +0700

    nop bai tap

--- Assignment25/ex2.js
+++ Assignment25/ex2.js
@@ -2,9 +2,9 @@
 function showTime() {
   setInterval(() => {
     const now = new Date();
-    const hours = String(now.getHours()).padStart(2, "0");
-    const minutes = String(now.getMinutes()).padStart(2, "0");
-    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const hours = `${now.getHours()}`.padStart(2, "0");
+    const minutes = `${now.getMinutes()}`.padStart(2, "0");
+    const seconds = `${now.getSeconds()}`.padStart(2, "0");
     console.log(`Thời gian hiện tại là: ${hours}:${minutes}:${seconds}`);
   }, 1000);
 }
--- Assignment25/ex4.js
+++ Assignment25/ex4.js
@@ -2,8 +2,8 @@
 function countDownNewYear() {
   const now = new Date();
   const newYearDate = new Date("2025-01-01");
-  const timeDifference = newYearDate.getTime() - now.getTime();
-  const daysLeft = timeDifference / (1000 * 60 * 60 * 24);
+  const dayNewYear = newYearDate.getTime() - now.getTime();
+  const daysLeft = dayNewYear / (1000 * 60 * 60 * 24);
   console.log(
     `Số ngày còn lại đến Tết Dương Lịch 2025: ${daysLeft.toFixed(0)} ngày`
   );
